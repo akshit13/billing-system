6e0e39d48c29123cc91a44b1fca02b4188cb863f
akshit13 <akshit1564@gmail.com>
2017-11-27 18:30:41 +0530

    Initital commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Billing-system
+
+A Cross platform desktop app built using [electron](https://electronjs.org/)  for generating and printing invoice.  
+
+
+## Compiling
+1. Install dependencies, "npm isntall"
+2. For compiling the application for win32 plateform, use  "electron-packager . --overwrite --asar=true --platform=win32 --arch=ia32"
+
+
+## Installing
+
+1. [Download](https://goo.gl/WbTMey) the compiled project for Windows
+2. Extract and execute the binary file `kgpoint.exe` to run the program.
+
---
+++ js/index.js
@@ -0,0 +1,89 @@
+function validateCname() {
+    var val = document.getElementById('cname').value;
+    if (!val.match(/^[a-zA-Z ]+$/)) {
+        return false;
+    }
+    return true;
+}
+
+var rowCount = 1;
+var totalBalance = 0;
+function calculate() {
+	localStorage["cname"] = document.getElementById('cname').value;
+	localStorage["discountid"] = document.getElementById('etdiscount').value;
+	var total, totals = 0, price, quantity;
+	for (var i = 1; i < rowCount; i++) {
+			price = document.getElementById('p' + i).value;
+			quantity = document.getElementById('q' + i).value;
+			item = document.getElementById('items'+i).value;
+			localStorage['q'+i] = quantity;
+			localStorage['p'+i] = price;
+			localStorage['items'+i] = item;
+			total = 1*price*quantity;
+			localStorage['total'+i] = total;
+			totals+=total;
+	}
+	document.getElementById('total').innerHTML = totals;
+	totalBalance = totals;
+	localStorage['rowCount'] = rowCount;
+}
+function addItem(){
+	var itemArray = new Array('Item1', 'Item2', 'Item3', 'Item4', 'Item5', 'Item6', 'Item7');
+	var table = document.getElementById('myTable');
+	var row = table.insertRow(rowCount);
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+	var cell3 = row.insertCell(2);
+	var cell4 = row.insertCell(3);
+	cell1.innerHTML = rowCount;
+	var select = document.createElement('select');
+	select.setAttribute('id', 'items'+rowCount)
+	select.setAttribute('class', 'col-sm-12 form-control')
+	cell2.innerHTML = "";
+	cell2.appendChild(select)
+	for (var i = 0; i < itemArray.length; i++) {
+		var option = document.createElement('option');
+		option.setAttribute('value', itemArray[i]);
+		option.text=itemArray[i];
+		select.appendChild(option);;
+	};
+	cell3.innerHTML = "<input type='number' id='q"+ rowCount +"' class='col-sm-12 form-control' value='1'>";
+	cell4.innerHTML = "<input type='number' id='p"+ rowCount +"' class='col-sm-10 form-control' placeholder='₹'>";
+	rowCount++;
+}
+addItem();
+
+function generateBill(){
+	if(validateCname()){
+		if (document.getElementById('cname').value==undefined || totalBalance == 0) {
+			alert('One Or More Fields can\'t be empty! \n please check Customer name or Price Value.\n\nPlease \'Calculate\' before generating Bill.');
+		}else{
+			location.href='raw/invoice.html';
+		}
+	}else{
+		alert('Sorry! Only alphabets(a-zA-Z) are allowed in Customer Name Field.');
+	}
+}
+
+window.onload = function(){
+
+	localStorage['isDiscount'] = 'false';
+	var dyes = document.getElementById('dyes');
+	var dno = document.getElementById('dno');
+
+	dyes.onclick = handle;
+	dno.onclick = handle;
+}
+
+function handle(){
+	var dyes = document.getElementById('dyes').checked;
+	var d = document.getElementById('etdiscount');
+
+	if (dyes) {
+		d.style.display="block";
+		localStorage['isDiscount'] = 'true';
+	}else{
+		d.style.display="none";
+		localStorage['isDiscount'] = 'false';
+	}
+}
